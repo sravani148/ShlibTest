312403f465e453f53db1981c185f11184efd61a5
sravani148 <60601937+sravani148@users.noreply.github.com>
2024-12-19 16:21:32 +0530

    Update scmcheckout.groovy

--- vars/scmcheckout.groovy
+++ vars/scmcheckout.groovy
@@ -1,7 +1,7 @@
 def call(def projectkey, def reponame, def branch)
 {
-  def gitID='xyz123'
-  baseurl=''
+  //def gitID='xyz123'
+  baseurl='https://github.com/'
   giturl="${baseurl}/${projectkey}/${reponame}.git"
   echo "Branch: "+branch;  
 }
