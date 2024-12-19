4716d216b4de5e690be86b505e21e1f8393a3d22
sravani148 <60601937+sravani148@users.noreply.github.com>
2024-12-19 18:09:01 +0530

    Update scmcheckout.groovy

--- vars/scmcheckout.groovy
+++ vars/scmcheckout.groovy
@@ -6,9 +6,4 @@ def errorMessage = ''
   giturl="${baseurl}/${projectkey}/${reponame}.git"
   echo "url : ${giturl}"
   echo "Branch: "+branch;  
-
-/*  def branchExists = script.sh(script:"git ls-remote --heads ${giturl} ${branch} | wc -l", returnStdout: true) as Integer
-  if (branchExists == 0 || params.branch?.trim().isEmpty()) {
-    errorMessage = "Branch not found"
-  } */
 }
