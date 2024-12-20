6c5afd175b45d6f7f54b67851e9025bc6e8556f9
sravani148 <60601937+sravani148@users.noreply.github.com>
2024-12-20 13:58:39 +0530

    Update mapparamtest.groovy

--- vars/mapparamtest.groovy
+++ vars/mapparamtest.groovy
@@ -3,4 +3,7 @@
         def reponame = params.containsKey('reponame') ? params.reponame : null
         def branch = params.containsKey('branch') ? params.branch : null
         def pathtodocker = params.containsKey('pathtodocker') ? params.pathtodocker : null
-    }
+  
+  echo "Reading params through map functionality: ${projectkey}/${reponame}/${branch}"
+  
+ }
